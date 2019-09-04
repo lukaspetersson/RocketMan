474ecfd2268aa0a78dbe7b83bea8be7134e7a096
Lukas <Lukas.petersson.1999@gmail.com>
2019-09-04 14:01:28 +0200

    layout and menu

--- app/src/main/java/com/example/rocketman/MainActivity.java
+++ app/src/main/java/com/example/rocketman/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.rocketman;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View rocket = findViewById(R.id.rocket);
+
+       /* ObjectAnimator animation = ObjectAnimator.ofFloat(rocket, "translationY", -1000f);
+        animation.setDuration(2000);
+        animation.start();*/
+    }
+
+    public void openSettings(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View v = inflater.inflate(R.layout.settings, null);
+
+        builder.setView(v);
+        builder.setTitle("edit parameters");
+        builder.setPositiveButton("Launch", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                launch();
+            }
+        });
+        builder.setNegativeButton("cancel", null);
+        builder.show();
+    }
+
+    public void launch(){
+
     }
 }
