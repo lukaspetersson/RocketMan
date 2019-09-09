4923c3096a57e2a5957cdfbec1ab225bd3e39531
Lukas <Lukas.petersson.1999@gmail.com>
2019-09-09 14:53:03 +0200

    sound spinner

--- app/src/main/java/com/example/rocketman/MainActivity.java
+++ app/src/main/java/com/example/rocketman/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,10 +14,16 @@ import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.BounceInterpolator;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    String nameSound = "Max";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +56,31 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
-    public void launch(final float height, final long time){
+
+        public void launch(final float height, final long time){
         final ImageView rocket = findViewById(R.id.rocket);
         final ImageView splash = findViewById(R.id.splash);
+        final Spinner soundEffect = findViewById(R.id.soundEffect);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.sound);
+        mp.start();
+
+        String[] items = new String[]{"Max", "Gustav", "Aron", "Lukas", "Jacob"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        soundEffect.setAdapter(adapter);
+
+        soundEffect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                nameSound = position + "";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         splash.setVisibility(View.VISIBLE);
 
@@ -65,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         splash.setVisibility(View.INVISIBLE);
+                        mp.stop();
                         rocket.animate()
                                 .translationY(0)
                                 .setDuration(time)
